e5d703ce9258eaf027f73e5992e7cae95d9e1ce0
kiet.nguyen-dang <kietnguyen17052001@gmail.com>
2023-03-10 15:44:21 +0700

    init project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"regexp"
+// )
+
+// func main() {
+// 	var email string
+// 	fmt.Print("input email:")
+// 	fmt.Scan(&email)
+// 	fmt.Println("Email:", email)
+// 	fmt.Println("Is valid email:", checkValidEmail(email))
+// }
+
+// func checkValidEmail(email string) bool {
+// 	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+// 	matched, _ := regexp.MatchString(pattern, email)
+// 	return matched
+// }
