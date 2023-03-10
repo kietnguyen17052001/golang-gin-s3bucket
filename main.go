73a1a6acd1a15165052a0c14be68e5289001f52d
kiet.nguyen-dang <kietnguyen17052001@gmail.com>
2023-03-10 16:49:37 +0700

    feat: api /upload (not s3)

--- main.go
+++ main.go
@@ -1,13 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
+	r.Static("/assets", "./assets")
+	r.LoadHTMLGlob("templates/*")
+	r.MaxMultipartMemory = 8 << 20 // 8MiB
+
+	// r.GET("/ping", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	r.GET("/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			// "title": "Main website",
 		})
 	})
+
+	r.POST("/upload", func(ctx *gin.Context) {
+		// Get file
+		file, err := ctx.FormFile("image")
+		log.Println("File name:", file.Filename)
+		if err != nil {
+			ctx.HTML(http.StatusOK, "index.html", gin.H{
+				"error": "Failed to upload image",
+			})
+			return
+		}
+
+		// Save file
+		err = ctx.SaveUploadedFile(file, "assets/uploads/"+file.Filename)
+
+		if err != nil {
+			ctx.HTML(http.StatusOK, "index.html", gin.H{
+				"error": "Failed to upload image",
+			})
+			return
+		}
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"image": "/assets/uploads/" + file.Filename,
+		})
+	})
+
 	r.Run()
 }
